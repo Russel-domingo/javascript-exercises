b83cd2dbd231e48c5131dc199926cc17653eb35d
Russel Domingo <russeldomingo099@gmail.com>
2025-09-18 15:07:18 +0800

    Finished 06_repeatingString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    if (times < 0) {
+        return 'ERROR';
+    }
+    let words = "";
 
+    for (let i = 0; i < times; i++) {
+        words += word;
+    }
+    return words;
 };
 
 // Do not edit below this line
