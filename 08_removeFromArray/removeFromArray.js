b9c05d6766e67c7c5d33abbdbb8dcd5022f31d07
Russel Domingo <russeldomingo099@gmail.com>
2025-09-18 15:33:04 +0800

    Finised 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...value){
+
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (value.includes(array[i]) != true) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
