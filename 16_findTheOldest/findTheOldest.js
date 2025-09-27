15ba589e377ac5e763a5f5198e892406da46f309
Russel Domingo <russeldomingo099@gmail.com>
2025-09-27 16:41:19 +0800

    Finished 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+    if(!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPeople) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPeople.yearOfBirth, currentPeople.yearOfDeath);
+
+        if (oldestAge < currentAge) {
+            return currentPeople;
+        }
+
+        return oldest;
+    });
 };
 
 // Do not edit below this line
