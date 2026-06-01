72f0a9f73b42d456d2c985ed785ca483a3225225
Russel Domingo <russeldomingo099@gmail.com>
2026-06-01 12:59:10 +0800

    finish exercise 2 contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,27 @@
-const contains = function() {
-  
+const contains = function(obj, value) {
+
+    const keys = Object.keys(obj);
+
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        const current = obj[key];
+
+        if (Number.isNaN(current) && Number.isNaN(value)) {
+            return true;
+        }
+
+        if (current === value) {
+            return true;
+        }
+
+        if (typeof current === "object" && current !== null) {
+            if (contains(current, value)) {
+                return true;
+            }
+        }
+    }
+
+    return false;
 };
   
 // Do not edit below this line
