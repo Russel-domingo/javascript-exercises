4fe9c543fcdb2f4eade394113b61459d11cd8ca2
Russel Domingo <russeldomingo099@gmail.com>
2026-06-01 13:57:19 +0800

    Finished total integer

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,5 +1,26 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(obj) {
+    
+    
+
+    if (typeof obj !== "object" || obj === null) {
+        return undefined;
+    }
+
+    const keys = Object.keys(obj);
+    let count = 0;
+
+    for (let i = 0; i < keys.length; i++) {
+        const value = obj[keys[i]];
+        if(Number.isInteger(value) ) {
+            count++;
+        } else if (typeof value === "object" && value !== null){
+            count += totalIntegers(value);
+        }
+    }
+
+    return count;
+
+
 };
   
 // Do not edit below this line
