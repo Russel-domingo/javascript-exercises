89cf3aae3e603355675b92e3e622cd9903040437
Russel Domingo <russeldomingo099@gmail.com>
2026-06-02 15:55:36 +0800

    Finished the js exercise PERMUTATION

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,5 +1,28 @@
-const permutations = function() {
-  
+const permutations = function(arr) {
+
+    if (arr.length === 0) {
+        return [[]];
+    }
+    if (arr.length === 1) {
+        return [arr];
+    }
+
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        const current = arr[i];
+
+        const remaining = arr.slice(0, i).concat(arr.slice(i + 1));
+
+        const perms = permutations(remaining);
+
+        for (let j = 0; j < perms.length; j++) {
+            result.push([current, ...perms[j]]);
+        }
+
+    }
+
+    return result;
 };
   
 // Do not edit below this line
