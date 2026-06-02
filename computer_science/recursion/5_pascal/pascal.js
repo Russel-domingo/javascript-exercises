5634667ca99bbd97b9e50d8c18d560a77fa96c1a
Russel Domingo <russeldomingo099@gmail.com>
2026-06-02 16:13:11 +0800

    Finished the js exercise pascal

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,22 @@
-const pascal = function() {
+const pascal = function(n) {
   
+    if (n == 1) {
+        return [1];
+    }
+
+    const previousRow = pascal(n - 1);
+
+    const newRow = [1];
+
+    for(let i = 0; i < previousRow.length - 1; i++) {
+        newRow.push(previousRow[i] + previousRow[i + 1]);
+    }
+
+    newRow.push(1);
+
+    return newRow;
+
+
 };
   
 // Do not edit below this line
